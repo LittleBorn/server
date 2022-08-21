96a685922c0069e6b0e310c463602ea78bc1483e
pafinkbeiner <paul-d-f@web.de>
2022-08-21 18:33:46 +0200

    removed dist

--- dist/utils/connectionHelper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.agent = void 0;
-const https = require("https");
-// At request level
-exports.agent = new https.Agent({
-    rejectUnauthorized: false,
-});
-//# sourceMappingURL=connectionHelper.js.map
